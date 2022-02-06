bb59f0ef9c1f649271b34bd6d7f2462a7d83a5ea
FSOsuit <druzhba2@inbox.lv>
2022-02-06 17:46:23 +0200

    created basic operators functions

---
+++ scripts.js
@@ -0,0 +1,19 @@
+//adding
+function add(a, b) {
+  return a + b;
+}
+
+//subtraction
+function subtract(a, b) {
+  return a - b;
+}
+
+//multiplication
+function multiply(a, b) {
+  return a * b;
+}
+
+//division
+function divide(a, b) {
+  return a / b;
+}
