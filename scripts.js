49bc79cc72a084da9c1ab75ef966014bf5562f78
FSOsuit <druzhba2@inbox.lv>
2022-02-13 11:46:53 +0200

    works with just 1 pair of numbers

--- scripts.js
+++ scripts.js
@@ -20,21 +20,32 @@ function divide(a, b) {
 
 function operate(operator, num1, num2 ) {
   if (operator === "+") {
-    console.log(add(num1, num2));
+    return add(num1, num2);
   } else if (operator === "-") {
-    console.log(subtract(num1, num2)); 
+    return subtract(num1, num2); 
   } else if (operator === "*") {
-    console.log(multiply(num1, num2));
+    return multiply(num1, num2);
   } else if (operator === "/") {
-    console.log(divide(num1, num2));
+    return divide(num1, num2);
   }
 }
 
+const operatorArray = [];
+const numberArray = [];
+
+let operatorCount = 0;
+let operatorSign = ""; //store clicke operator sign
+let number1 = 0; // stores 1st number
+let number2 = 0; // stores 2nd number
 let clicked = ""; // stores value of clicked num button
 let display = document.querySelector('.display');
 
 function populateDisplay () {
-  display.textContent += clicked;
+  if (display.textContent.length < 1) {
+    display.textContent = clicked;
+  } else {
+    display.textContent += clicked;
+  }
 }
 
 
@@ -42,8 +53,31 @@ function populateDisplay () {
 const numbers = document.querySelectorAll('.number');
 numbers.forEach((number) => {
   number.addEventListener('click', () => {
-    
     clicked = number.textContent; //stores clicked value
     populateDisplay();
   });
 });
+
+//adds event listener to operator buttons, stores 1st number, resets display, saves clicked operator in variable
+const operators = document.querySelectorAll('.operator');
+operators.forEach((operator) => {
+  operator.addEventListener('click', () => {
+    operatorSign = operator.textContent;
+    number1 = display.textContent;
+    display.textContent = "";
+  });
+});
+
+const equal = document.querySelector('.equals');
+equal.addEventListener('click', () => {
+  number2 = display.textContent;
+  display.textContent = operate(operatorSign, Number(number1), Number(number2));
+});
+
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', () => {
+  number1 = "";
+  number2 = "";
+  display.textContent = "";
+});
+
